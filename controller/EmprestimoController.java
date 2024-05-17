537d5a6a2cae42854775a7a360e0d1d450a9c0d5
DiogoMakotto <diogomakotto@gmail.com>
2024-05-16 22:21:08 -0300

    feat: Criando camadas Controller

---
+++ controller/EmprestimoController.java
@@ -0,0 +1,37 @@
+package br.com.diogomakotto.emprestimoapi.controller;
+
+import br.com.diogomakotto.emprestimoapi.entity.Emprestimo;
+import br.com.diogomakotto.emprestimoapi.service.EmprestimoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/emprestimos")
+public class EmprestimoController {
+    private EmprestimoService emprestimoService;
+
+    public EmprestimoController(EmprestimoService emprestimoService) {
+        this.emprestimoService = emprestimoService;
+    }
+
+    @PostMapping
+    List<Emprestimo> create(@RequestBody Emprestimo emprestimo){
+        return emprestimoService.create(emprestimo);
+    }
+
+    @GetMapping
+    List<Emprestimo> list(){
+        return emprestimoService.list();
+    }
+
+    @PutMapping
+    List<Emprestimo> update(@RequestBody Emprestimo emprestimo){
+        return emprestimoService.update(emprestimo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Emprestimo> delete(@PathVariable("id") Long id){
+        return emprestimoService.delete(id);
+    }
+}
