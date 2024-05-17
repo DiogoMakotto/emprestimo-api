4bd1f05a6825cc2ee62327c5207847ba54539018
DiogoMakotto <diogomakotto@gmail.com>
2024-05-16 21:47:51 -0300

    feat: Criando Entity

---
+++ entity/Fintech.java
@@ -0,0 +1,46 @@
+package br.com.diogomakotto.emprestimoapi.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="emprestimos")
+public class Fintech {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String valorEmprestimoDesejado;
+    private String rendaMensal;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getValorEmprestimoDesejado() {
+        return valorEmprestimoDesejado;
+    }
+
+    public void setValorEmprestimoDesejado(String valorEmprestimoDesejado) {
+        this.valorEmprestimoDesejado = valorEmprestimoDesejado;
+    }
+
+    public String getRendaMensal() {
+        return rendaMensal;
+    }
+
+    public void setRendaMensal(String rendaMensal) {
+        this.rendaMensal = rendaMensal;
+    }
+}
